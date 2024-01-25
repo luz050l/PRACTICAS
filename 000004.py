f2b1883932c17fc371477c38800cdd804f88fce4
luz050l <149832493+luz050l@users.noreply.github.com>
2024-01-24 22:40:12 -0600

    Add files via upload

---
+++ 000004.py
@@ -0,0 +1,11 @@
+def restaSP():
+    n1=int(input("dame el numero 1:"))
+    n2=int(input("dame el numero 2:"))
+    r=n1-n2
+    print("el resultado es:",r)
+restaSP()
+
+def restaCP(n3,n4):
+    r=n3-n4
+    print("el resultado es:",r)
+restaCP(8,4)
