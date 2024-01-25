04a1b41198928ba4c52b06f8f4c884f0a256255c
luz050l <149832493+luz050l@users.noreply.github.com>
2024-01-24 22:56:04 -0600

    Add files via upload

---
+++ 000006.py
@@ -0,0 +1,11 @@
+def sumaSP():
+    n1=int(input("dame el numero 1:"))
+    n2=int(input("dame el numero 2:"))
+    r=n1+n2
+    print("el resultado es:",r)
+sumaSP()
+
+def sumaCP(n3,n4):
+    rs=n3+n4
+    print("el sultado es:",rs)
+sumaCP
