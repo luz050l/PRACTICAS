dcca900349c76fdf0dabf4b87b2e70fd4c74d127
luz050l <149832493+luz050l@users.noreply.github.com>
2024-01-24 22:22:31 -0600

    Add files via upload

---
+++ 000002.py
@@ -0,0 +1,11 @@
+def divisionSP():
+    n1=int(input("dame el numero 1: "))
+    n2=int(input("dame el numero 2: "))
+    r= n1/n2
+    print("el resultado es:", r)
+divisionSP()
+
+def divisionCP(n3,n4):
+    r=n3/n4
+    print("el resultado es:",r)
+divisionCP(12,3)
