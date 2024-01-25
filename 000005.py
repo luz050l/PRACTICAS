a21cb179a70aa4387a6357626d5478e20ec6c158
luz050l <149832493+luz050l@users.noreply.github.com>
2024-01-24 22:44:26 -0600

    Add files via upload

---
+++ 000005.py
@@ -0,0 +1,10 @@
+def saludosSP():
+    nombre=input("nombre: ")
+    saludo=input("buenos dias")
+    print("saludo")
+saludosSP()
+
+def saludo(nombre):
+    saludo=("buenos dias", nombre)
+    print(saludo)
+saludo("robert")
