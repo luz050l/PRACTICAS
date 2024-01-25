29c69d44fde484a885133ffc52a6c46797201c82
luz050l <149832493+luz050l@users.noreply.github.com>
2024-01-24 22:36:06 -0600

    Add files via upload

---
+++ 000003.py
@@ -0,0 +1,12 @@
+def multiplicacionSP():
+    n1=int(input("dame el numero 1: "))
+    n2=int(input("dame el numero 2: "))
+    r=n1*n2
+    print("el resultado es:",r)
+multiplicacionSP()
+
+
+def multiplicacionCP(n3,n4):
+    r=n3*n4
+    print("el resultado es:",r)
+multiplicacionCP(5,5)
